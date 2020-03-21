b1e7e63a6a1dab49c88669b243bb0a3b7243be3a
Manuela Ritter <manritter@uni-osnabrueck.de>
2020-03-21 16:36:32 +0100

    landing page start + buttons

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="pane pane-1">
+        <div className="pane-container">
+          <h1>Digitalisierungswichtel</h1>
+          <p>Was machen wir?</p>
+        </div>
+      </div>
+      <div className="pane pane-2">
+        <div className="pane-container">
+          <h1 className="mt-3">Ich bin Händler, wie starte ich?</h1>
+          <p>Als Händler xy, tu ich das zuerst</p>
+          <button className="btn btn-home">Quiz starten</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
