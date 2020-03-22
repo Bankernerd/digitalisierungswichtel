2f4327254a4aa38bb6fb14e19d950e2176a1461e
Lennart Erpenbeck <l.erpenbeck@salt-and-pepper.eu>
2020-03-22 01:01:25 +0100

    answer flow

--- src/Frage/index.js
+++ src/Frage/index.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 
 import { Card } from './Card';
 
 function Frage() {
   const [current, setCurrent] = React.useState(0);
 
-  const fragen = {};
+  const fragen = [{
+          frage: 'Welche Art von Ware bietst du an',
+          answers: ['Lebensmittel', 'Produkte', 'Dienstleistung'],
+        },
+        {
+          frage: 'Arbeitest du mit Terminen?',
+          answers: ['Ja, Telefonisch', 'Ja per Webportal', 'Nein'],
+        },
+        {
+          frage: 'Hast du einen Lieferservice?',
+          answers: ['Ja', 'Nein'],
+        },
+        {
+          frage: 'Wie erreichst du deinen Kunden?',
+          answers: ['Persönlicher Kontak', 'Social Media', 'Print Werbung'],
+        }];
+
+  function inc()
+  {
+    setCurrent(current + 1);
+  };
 
   return (
     <div className="frage">
       <div className="question-pane">
-        <Card
-          frage={'Was verkaufst du so?'}
-          answers={['Essen', 'Services', 'mich', 'Klopapier']}
-        />
+        {current < fragen.length ? (
+          <Card
+            frage={fragen[current].frage}
+            answers={fragen[current].answers}
+            onAnswerSelected={inc}
+          />
+        ):(
+          <div>"You did it"</div>
+        )}
       </div>
     </div>
   );
